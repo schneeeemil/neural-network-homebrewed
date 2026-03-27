2ca2fe982d55601ae51c6352c45971c800f0fcb9
emil.s.nebelung@gmail.com <>
2026-03-27 19:10:34 +0100

    Initial commit

---
+++ src/main/java/org/nebelung/neural/network/homebrewed/NeuralNetworkHomebrewed.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package org.nebelung.neural.network.homebrewed;
+
+/**
+ *
+ * @author emils
+ */
+public class NeuralNetworkHomebrewed {
+
+    public static void main(String[] args) {
+        System.out.println("Moin");
+    }
+}
